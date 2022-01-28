debb4be758ba8a130fa10aed6b045b99a29db0d6
Jérôme TAMA <j.tama@groupeonepoint.com>
2022-01-28 15:54:22 +0100

    feat: Install using helm chart

--- operator/src/main/java/com/onepoint/sdl/ri/RandomInvasionRequest.java
+++ operator/src/main/java/com/onepoint/sdl/ri/RandomInvasionRequest.java
@@ -5,7 +5,7 @@ import io.fabric8.kubernetes.model.annotation.Group;
 import io.fabric8.kubernetes.model.annotation.ShortNames;
 import io.fabric8.kubernetes.model.annotation.Version;
 
-@Group("yap.onepoint.com")
+@Group("sld.onepoint.com")
 @Version("v1alpha1")
 @ShortNames("rir")
 public class RandomInvasionRequest extends RandomRequest {
